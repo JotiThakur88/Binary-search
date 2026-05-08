5808a502661456b4a10495a34b173f8453d3be03
Jyoti <151373033+JotiThakur88@users.noreply.github.com>
2026-05-08 18:26:10 +0530

    Time: 0 ms (100%), Space: 31.2 MB (73.86%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        // Perfect binary tree
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+
+private:
+    int getLeftHeight(TreeNode* node) {
+        int height = 0;
+
+        while (node) {
+            height++;
+            node = node->left;
+        }
+
+        return height;
+    }
+
+    int getRightHeight(TreeNode* node) {
+        int height = 0;
+
+        while (node) {
+            height++;
+            node = node->right;
+        }
+
+        return height;
+    }
+};
