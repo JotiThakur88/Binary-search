a074ba549bfdbcd115159f6ce52d168fe011aa4b
Jyoti <151373033+JotiThakur88@users.noreply.github.com>
2026-05-08 18:41:28 +0530

    Time: 0 ms (100%), Space: 70.7 MB (28.78%) - LeetHub

---
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+
+        int n = arr.size();
+
+        // Find longest sorted prefix
+        int left = 0;
+
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left++;
+        }
+
+        // Already sorted
+        if (left == n - 1)
+            return 0;
+
+        // Find longest sorted suffix
+        int right = n - 1;
+
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+            right--;
+        }
+
+        // Remove prefix or suffix
+        int ans = min(n - left - 1, right);
+
+        // Merge prefix and suffix
+        int i = 0;
+        int j = right;
+
+        while (i <= left && j < n) {
+
+            if (arr[i] <= arr[j]) {
+                ans = min(ans, j - i - 1);
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
